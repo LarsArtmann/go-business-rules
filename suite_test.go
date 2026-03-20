d49311fd8278ba26c80588cc56010829f09a99b7
Lars Artmann <git@lars.software>
2026-03-20 15:08:42 +0100

    test: update version constant test to 1.1.0
    Assisted-by: MiniMax-M2.7-highspeed via Crush <crush@charm.land>

--- suite_test.go
+++ suite_test.go
@@ -117,7 +117,7 @@ var _ = Describe("Core Types", func() {
 
 	Describe("Version", func() {
 		It("should have a version constant", func() {
-			Expect(businessrules.Version).To(Equal("1.0.0"))
+			Expect(businessrules.Version).To(Equal("1.1.0"))
 		})
 	})
 })
